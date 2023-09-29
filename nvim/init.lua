205c3f0afef2d220e904652af66f748e52f6ab45
Matthew T. Smith <smithmts+github@gmail.com>
2023-09-28 23:33:30 -0700

    Fixed nvim directory
    nvim folder was previously its own repository.  Had deleted .git folder,
    but I wasn't aware of cached data and it committed as a "submodule."
    Deleted cached data, migrated .gitignore to the repo root directory.

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+-- Set leader key. '\\' is really '\' to escape the escape.
+vim.g.mapleader = '\\'
+vim.g.maplocalleader = '\\'
+
+-- Source files in lua/mts/
+require "mts.lazy"
+require "mts.options"
+require "mts.treesitter"
+require "mts.lsp_config"
+require "mts.keymaps"
+--require "mts.packer"
---
+++ nvim/lua/mts/lazy.lua
@@ -0,0 +1,50 @@
+-- Install package manager
+--    https://github.com/folke/lazy.nvim
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Begin installation of plugins
+require('lazy').setup({
+
+	{ -- Treesitter to highlight, edit, and navigate code
+		'nvim-treesitter/nvim-treesitter',
+		build = ":TSUpdate", -- kickstart.nvim used a `configure` function, which seemed to be triggring "All parcers..." message at startup.
+   	 	dependencies = {
+		'nvim-treesitter/nvim-treesitter-textobjects',
+   	 	},
+	},
+
+	-- Colorscheme(s) supporting treesiter
+ 	'Mofiqul/adwaita.nvim',
+
+	{ -- LSP Configuration & Plugins
+		'neovim/nvim-lspconfig',
+		dependencies = {
+		-- Automatically install LSPs to stdpath for neovim
+		'williamboman/mason.nvim',
+		'williamboman/mason-lspconfig.nvim',
+
+		-- Useful status updates for LSP
+		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+		-- { 'j-hui/fidget.nvim', opts = {} },
+
+		-- Additional lua configuration, makes nvim stuff amazing!
+		'folke/neodev.nvim',
+		},
+	},
+
+	-- which-key: Useful plugin to show you pending keybinds.
+	{ 'folke/which-key.nvim', opts = {} },
+
+}) --End plugin installs
+		vim.g.adwaita_darker = true -- darker theme version
